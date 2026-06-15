9efabe21b5dabf209c6a9d97240939f867c4d911
Alejandro-Emanuel <emanuel.currais@outlook.com>
2026-06-14 21:44:33 -0300

    adicao do icone base

--- src/componentes/CardMonitoramento.tsx
+++ src/componentes/CardMonitoramento.tsx
@@ -13,9 +13,9 @@ export function CardMonitoramento({ litros, volume }: CardMonitoramentoProps) {
       {/* Container da Esquerda: Imagem Quadrada e Responsiva */}
       <View style={styles.containerImagem}>
         <Image
-          source={{
-            uri: 'https://imgs.search.brave.com/_Lep-XudKASpVr1USJyxWEr5NjCGXQP-y36o32auZtA/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/Zm90b3MtZ3JhdGlz/L2Vub3JtZS10YW5x/dWUtZGUtcGxhc3Rp/Y28tcGFyYS1hZ3Vh/LWlzb2xhZGEtbm8t/YnJhbmNvXzkzNjc1/LTEzNTIzOC5qcGc_/c2VtdD1haXNfaHli/cmlk',
-          }}
+          source={
+            require('../../assets/Icone0.jpg')
+          }
           style={styles.caixaDaguaImg}
           resizeMode="cover"
         />
