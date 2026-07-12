5165f550d917506263e0172045ecd65923602d11
Jhonata-502 <jhonatapamancio@gmail.com>
2026-07-12 15:33:13 -0300

    Notificação de merda

--- src/navigation/AppTabs.tsx
+++ src/navigation/AppTabs.tsx
@@ -2,40 +2,82 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { DashboardScreen } from '../screens/DashboardScreen';
 import { EstatisticasScreen } from '../screens/EstatisticasScreen';
+import { NotificacoesScreen } from '../screens/NotificacaoScreen';
+import { Pressable, View } from 'react-native';
 
 export type RootTabParamList = {
   Dashboard: undefined;
   Estatistica: undefined;
+  Notificacoes: undefined;
 };
-
 const Tab = createBottomTabNavigator<RootTabParamList>();
 
 function obterIcone(nomeRota: keyof RootTabParamList, focused: boolean) {
-  if (nomeRota === 'Dashboard') return focused ? 'home' : 'home-outline';
-  return focused ? 'bar-chart' : 'bar-chart-outline';
+  switch (nomeRota) {
+    case 'Dashboard':
+      return focused ? 'home' : 'home-outline';
+
+    case 'Estatistica':
+      return focused ? 'bar-chart' : 'bar-chart-outline';
+
+    case 'Notificacoes':
+      return focused ? 'notifications' : 'notifications-outline';
+  }
 }
 
 export function AppTabs() {
   return (
-    <Tab.Navigator
-      initialRouteName="Dashboard"
-      screenOptions={({ route }) => ({
-        headerStyle: { backgroundColor: '#0f172a' },
-        headerTintColor: '#f8fafc',
-        tabBarActiveTintColor: '#00ABE4',
-        tabBarInactiveTintColor: '#64748b',
-        tabBarStyle: { height: 62, paddingBottom: 8, paddingTop: 6 },
-        tabBarIcon: ({ focused, color, size }) => (
-          <Ionicons
-            name={obterIcone(route.name as keyof RootTabParamList, focused)}
-            color={color}
-            size={size}
-          />
-        ),
-      })}
-    >
-      <Tab.Screen name="Dashboard" component={DashboardScreen} options={{ title: 'Principal' }} />
+   <Tab.Navigator
+  initialRouteName="Dashboard"
+  screenOptions={({ route, navigation }) => ({
+    headerStyle: { backgroundColor: '#0f172a' },
+    headerTintColor: '#f8fafc',
+
+    headerRight: () => (
+      <Pressable
+        onPress={() => navigation.navigate('Notificacoes')}
+        style={{
+          marginRight: 16,
+          position: 'relative',
+        }}
+      >
+        <Ionicons
+          name="notifications-outline"
+          size={28}
+          color="#00ABE4"
+        />
+
+        {/* Bolinha vermelha */}
+        <View
+          style={{
+            position: 'absolute',
+            top: -2,
+            right: -2,
+            width: 10,
+            height: 10,
+            borderRadius: 5,
+            backgroundColor: 'red',
+          }}
+        />
+      </Pressable>
+    ),
+
+    tabBarActiveTintColor: '#00ABE4',
+    tabBarInactiveTintColor: '#64748b',
+    tabBarStyle: { height: 62, paddingBottom: 8, paddingTop: 6 },
+
+    tabBarIcon: ({ focused, color, size }) => (
+      <Ionicons
+        name={obterIcone(route.name as keyof RootTabParamList, focused)}
+        color={color}
+        size={size}
+      />
+    ),
+  })}
+>
+       <Tab.Screen name="Dashboard" component={DashboardScreen} options={{ title: 'Principal' }} />
       <Tab.Screen name="Estatistica" component={EstatisticasScreen} options={{ title: 'Estatisticas' }} />
+      <Tab.Screen name="Notificacoes"component={NotificacoesScreen}options={{title: 'Notificações',tabBarButton: () => null,  }}/>
     </Tab.Navigator>
   );
 }
--- src/screens/DashboardScreen.tsx
+++ src/screens/DashboardScreen.tsx
@@ -1,3 +1,4 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
 import type { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
 import { DrawerActions } from '@react-navigation/native';
 import { Pressable, Text, View } from 'react-native';
@@ -8,13 +9,29 @@ import { styles } from '../styles';
 type Props = BottomTabScreenProps<RootTabParamList, 'Dashboard'>;
 
 export function DashboardScreen({ navigation }: Props) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.titulo}>Caixa principal</Text>
-      <Text style={styles.subtitulo}>Estado da sua caixa d'gua</Text>
+ return (
+  <View style={styles.container}>
 
-      <CardMonitoramento />
-      
+    <View
+      style={{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 20,
+      }}
+    >
+      <View>
+        <Text style={styles.titulo}>Caixa principal</Text>
+        <Text style={styles.subtitulo}>
+          Estado da sua caixa d'água
+        </Text>
+      </View>
+
+  
+
     </View>
-  );
-}
+
+    <CardMonitoramento />
+
+  </View>
+);}
